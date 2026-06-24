632409e4d501ad4ddeaf0b7e062daab6dc7102a2
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-24 11:40:52 +0300

    css_vars => _

--- apps/Client/src/features/projects/reducer/actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { AppTypeT } from '.';
-import { ProjectT } from '@/common/types/projects';
-
-export const ProjectsActT = {
-  SET_APP_TYPE: createAction('SET_APP_TYPE', props<{ appType: AppTypeT[] }>()),
-  SET_PAGE: createAction('SET_PAGE', props<{ page: number }>()),
-};
--- apps/Client/src/features/projects/reducer/selector.ts
+++
@@ -1,5 +0,0 @@
-import { createFeatureSelector } from '@ngrx/store';
-import { ProjectsStateT } from '.';
-
-export const getProjectsState = createFeatureSelector<ProjectsStateT>('projects');
-
