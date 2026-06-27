7ef87e5932d3a2b5f4a6bdf8eac04e16c211e446
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-27 13:20:37 +0300

    homepage_mobile => _

---
+++ apps/Client/src/core/lib/data_structure/formatter.ts
@@ -0,0 +1,9 @@
+export class FormatterLib {
+  public static formatDate(date: string) {
+    return new Intl.DateTimeFormat('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    }).format(new Date(date));
+  }
+}
--- apps/Client/src/pages/home_page/home-page.ts
+++ apps/Client/src/pages/home_page/home-page.ts
@@ -2,10 +2,13 @@ import { UseThemeSvc } from '@/core/services/use_theme';
 import { NgClass, NgComponentOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { HomepageUiFct } from './ui_fct';
+import { BlogsUiFct } from '@/features/blogs/ui_fct';
+import { FormatterLib } from '@/core/lib/data_structure/formatter';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NgClass, NgComponentOutlet],
+  imports: [NgClass, NgComponentOutlet, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,4 +17,9 @@ export class HomePage {
   public readonly useThem: UseThemeSvc = inject(UseThemeSvc);
 
   public readonly HomepageUiFct = HomepageUiFct;
+  public readonly BlogsUiFct = BlogsUiFct;
+
+  public formatDate(date: string) {
+    return FormatterLib.formatDate(date);
+  }
 }
