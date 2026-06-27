c60bdd5057830a8a26c4f3f58ba93574e4a007e1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-27 13:03:06 +0300

    homepage_mobile => _

--- apps/Client/src/pages/home_page/home-page.ts
+++ apps/Client/src/pages/home_page/home-page.ts
@@ -1,14 +1,17 @@
 import { UseThemeSvc } from '@/core/services/use_theme';
-import { NgClass } from '@angular/common';
+import { NgClass, NgComponentOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HomepageUiFct } from './ui_fct';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NgClass],
+  imports: [NgClass, NgComponentOutlet],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
   public readonly useThem: UseThemeSvc = inject(UseThemeSvc);
+
+  public readonly HomepageUiFct = HomepageUiFct;
 }
